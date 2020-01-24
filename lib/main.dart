135f280d3d7dcb87c3ae9d63ec567f9b61a640c0
jhonsnoww <oochanbg@gmail.com>
2020-01-24 09:57:30 +0630

    Ready To View

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/repositories/weather_api_client.dart';
+import 'package:weather_app/repositories/weather_repositories.dart';
+import 'package:weather_app/widgets/simple_bloc_delegate.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/widgets/weather_view.dart';
+
+import 'blocs/weather_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherApiClient: WeatherApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+  runApp(
+    MyApp(
+      repository: weatherRepository,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  final WeatherRepository repository;
+  MyApp({Key key, @required this.repository})
+      : assert(repository != null),
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Weather App',
+      home: BlocProvider(
+        create: (context) => WeatherBloc(weatherRepository: repository),
+        child: WeatherView(),
+      ),
+    );
   }
 }
---
+++ lib/widgets/weather_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WeatherView extends StatefulWidget {
+  @override
+  _WeatherViewState createState() => _WeatherViewState();
+}
+
+class _WeatherViewState extends State<WeatherView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
